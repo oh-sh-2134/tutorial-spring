89fbea1727e31f270683ee939d93fe5dc29cde28
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-21 00:26:08 +0900

    add findAll test1

--- src/test/java/tutorial/tutorialspring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/tutorial/tutorialspring/repository/MemoryMemberRepositoryTest.java
@@ -30,13 +30,44 @@ class MemoryMemberRepositoryTest {
 
     @Test
     void findById() {
+        // save에서 이미 검증함
     }
 
     @Test
     void findByName() {
+        //given
+        Member member = new Member();
+        member.setName("osh");
+
+        //when
+        repository.save(member);
+
+        //then
+        Member result = new Member();
+        if(repository.findByName(member.getName()).isPresent())
+            result = repository.findByName(member.getName()).get();
+
+        assertThat(result).isEqualTo(member);
     }
 
     @Test
     void findAll() {
+
+        //given
+        Member member1 = new Member();
+        member1.setName("name1");
+
+
+        Member member2 = new Member();
+        member2.setName("name2");
+
+
+        //when
+        repository.save(member1);
+        repository.save(member2);
+
+
+        //then
+        assertThat(repository.findAll().size()).isEqualTo(2);
     }
 }
