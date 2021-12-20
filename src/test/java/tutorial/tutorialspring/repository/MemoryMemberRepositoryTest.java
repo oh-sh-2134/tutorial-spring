8c503e415d43f33ce7e41e8159131176bc12fa72
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-20 23:12:42 +0900

    init MemoryMemberRepositoryTest

---
+++ src/test/java/tutorial/tutorialspring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,24 @@
+package tutorial.tutorialspring.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
