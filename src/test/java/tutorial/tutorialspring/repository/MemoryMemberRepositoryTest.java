626e6260e5b523a80a9653c35969ed1dceafcbe6
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-21 00:15:51 +0900

    add save test1

--- src/test/java/tutorial/tutorialspring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/tutorial/tutorialspring/repository/MemoryMemberRepositoryTest.java
@@ -1,13 +1,31 @@
 package tutorial.tutorialspring.repository;
 
 import org.junit.jupiter.api.Test;
+import tutorial.tutorialspring.domin.Member;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 class MemoryMemberRepositoryTest {
 
+    MemberRepository repository = new MemoryMemberRepository();
+
     @Test
     void save() {
+
+        //given
+        Member member = new Member();
+        member.setName("osh");
+
+        //when
+        repository.save(member);
+
+        //then
+        Member result = new Member();
+        if(repository.findById(member.getId()).isPresent())
+            result = repository.findById(member.getId()).get();
+
+        assertThat(result).isEqualTo(member);
     }
 
     @Test
