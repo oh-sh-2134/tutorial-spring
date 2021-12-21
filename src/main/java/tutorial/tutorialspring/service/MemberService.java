1e68a700e175230dd986d18b7c9c5b8f1aaac663
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-21 20:52:59 +0900

    MemberRepository가 MemberService를 DI 하게 변경

--- src/main/java/tutorial/tutorialspring/service/MemberService.java
+++ src/main/java/tutorial/tutorialspring/service/MemberService.java
@@ -10,12 +10,15 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
-        회원가입
-     */
+            회원가입
+         */
     public Long join(Member member){
         validateDuvalidateDuplicateMember(member);
         memberRepository.save(member);
