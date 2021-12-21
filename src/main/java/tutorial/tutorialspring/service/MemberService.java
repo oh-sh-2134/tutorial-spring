31e5bec00dc425fc67340a277b7cb114e8d37288
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-21 20:48:58 +0900

    add join(회원가입) , findMemebers(회원조회), findOne(회원조회)

--- src/main/java/tutorial/tutorialspring/service/MemberService.java
+++ src/main/java/tutorial/tutorialspring/service/MemberService.java
@@ -1,4 +1,37 @@
 package tutorial.tutorialspring.service;
 
+import tutorial.tutorialspring.domin.Member;
+import tutorial.tutorialspring.repository.MemberRepository;
+import tutorial.tutorialspring.repository.MemoryMemberRepository;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    /*
+        회원가입
+     */
+    public Long join(Member member){
+        validateDuvalidateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuvalidateDuplicateMember(Member member) {
+        memberRepository.findById(member.getId())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+
+    }
+    // 전체 회원 조회
+    public List<Member> findMembers(){ return memberRepository.findAll();}
+
+    // 회원 조회
+    public Optional<Member> findOne(Long MemberId) {return memberRepository.findById(MemberId);}
 }
