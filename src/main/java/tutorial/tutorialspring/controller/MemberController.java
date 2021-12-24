f45760da6679fa0b6f8e135be2e8bf41080ea070
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-24 20:45:08 +0900

    add createForm, create, list

--- src/main/java/tutorial/tutorialspring/controller/MemberController.java
+++ src/main/java/tutorial/tutorialspring/controller/MemberController.java
@@ -1,7 +1,46 @@
 package tutorial.tutorialspring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import tutorial.tutorialspring.domin.Member;
+import tutorial.tutorialspring.service.MemberService;
+
+import java.util.List;
+
 
 @Controller
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public  MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> membsers = memberService.findMembers();
+        model.addAttribute("members",membsers);
+        return "/members/memberList";
+    }
+
 }
