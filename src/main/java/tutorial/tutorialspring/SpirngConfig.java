b07c7f0714239797616f515c3357715800f58c92
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-12-27 23:47:20 +0900

    SpringConfig

--- src/main/java/tutorial/tutorialspring/SpirngConfig.java
+++ src/main/java/tutorial/tutorialspring/SpirngConfig.java
@@ -2,12 +2,21 @@ package tutorial.tutorialspring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tutorial.tutorialspring.repository.JdbcMemberRepository;
 import tutorial.tutorialspring.repository.MemberRepository;
 import tutorial.tutorialspring.repository.MemoryMemberRepository;
 import tutorial.tutorialspring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpirngConfig {
+
+    private final DataSource dataSource;
+
+    public SpirngConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +25,7 @@ public class SpirngConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
