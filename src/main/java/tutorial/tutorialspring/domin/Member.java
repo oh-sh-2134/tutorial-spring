06c1cf9c042e3abd8bfa5f9bab4cfc3c066bb4b3
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-01-05 23:47:30 +0900

    JPA

--- src/main/java/tutorial/tutorialspring/domin/Member.java
+++ src/main/java/tutorial/tutorialspring/domin/Member.java
@@ -1,7 +1,15 @@
 package tutorial.tutorialspring.domin;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private String name;
     private Long id;
 
