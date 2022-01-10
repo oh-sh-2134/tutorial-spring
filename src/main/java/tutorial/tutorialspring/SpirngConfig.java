59ee71a98f6ade0fc5f5b1c58a6cca3e45ddc521
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-01-10 23:45:39 +0900

    JPA

--- src/main/java/tutorial/tutorialspring/SpirngConfig.java
+++ src/main/java/tutorial/tutorialspring/SpirngConfig.java
@@ -2,21 +2,23 @@ package tutorial.tutorialspring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tutorial.tutorialspring.repository.JdbcMemberRepository;
 import tutorial.tutorialspring.repository.JdbcTemplateMemberRepository;
+import tutorial.tutorialspring.repository.JpaMemberRepository;
 import tutorial.tutorialspring.repository.MemberRepository;
-import tutorial.tutorialspring.repository.MemoryMemberRepository;
 import tutorial.tutorialspring.service.MemberService;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpirngConfig {
 
     private final DataSource dataSource;
+    private final EntityManager em;
 
-    public SpirngConfig(DataSource dataSource) {
+    public SpirngConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -28,6 +30,7 @@ public class SpirngConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
